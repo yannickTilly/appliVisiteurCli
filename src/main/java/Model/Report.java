34c4709e21c1db60cdf84bb4372f20a017cec9bd
Yannick <yannick13009@gmail.com>
2020-01-06 16:28:31 +0100

    fix localdate

--- src/main/java/Model/Report.java
+++ src/main/java/Model/Report.java
@@ -2,6 +2,10 @@ package Model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -10,6 +14,8 @@ import java.util.Collection;
 public class Report {
     private long id;
     private String description;
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate date;
     private User user;
