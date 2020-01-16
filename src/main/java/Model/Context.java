c6cddc71546161dc35014235d0cce6c2c81a7e39
Yannick <yannick13009@gmail.com>
2020-01-16 23:51:05 +0100

    get Role context

--- src/main/java/Model/Context.java
+++ src/main/java/Model/Context.java
@@ -1,6 +1,8 @@
 package Model;
 
 import Listener.ContextListener;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import javax.swing.event.EventListenerList;
 import java.awt.event.ActionListener;
@@ -19,12 +21,17 @@ public class Context {
         contextListeners = new ArrayList<>();
     }
     public String getToken() {
-        return token;
+        return token.toString();
     }
     public Context setToken(String token) {
         this.token = token;
         fireUserLoginSucess();
         return this;
+    }
+    public String getRole()
+    {
+        DecodedJWT decodedJWT = JWT.decode(token);
+        return decodedJWT.getClaim("role").asString();
     }
     public void addListener(ContextListener contextListener)
     {
